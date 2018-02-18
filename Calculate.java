301fbdd6b3f0a19bf8ab5f626467cf9d7ef9c3c5
Andrew Kamendrovsky <32849000+dizeldron@users.noreply.github.com>
2018-02-18 17:28:34 +0300

    Add files via upload

--- Calculate.java
+++ Calculate.java
@@ -1,111 +1,113 @@
- import java.io.IOException;
- import java.util.InputMismatchException;
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Calculate {
 
+    static Scanner sc = new Scanner(System.in);
 
-    private static int ReadInt(Scanner scanner) {
-        while (!scanner.hasNextInt()) {
-        }
-        return scanner.nextInt();
-    }
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws Exception {
-        int firstNum;
-        int secondNum;
-        int thirdNum;
-        int fourthNum;
-        int upperResult;
-        int lowerResult;
-        String operations;
+        System.out.println("Enter the numerator of the first fraction: ");
 
+        int firstNum = getInt();
+        System.out.println(firstNum + " :Is the numerator of the first fraction.");
+        System.out.println("   Enter the denominator of the first fraction: ");
 
-        Scanner sc = new Scanner(System.in);
+        int secondNum = getInt();
+        System.out.println(secondNum + " : Is the denominator of the first fraction.");
+        System.out.println("   Now the first fraction looks like this: " + firstNum + "/" + secondNum + ".");
+        System.out.println("   Enter the numerator of the second fraction: ");
 
-        System.out.println("WARNING!!! Input only integers, no exception handlers envisaged\n"
-                + "Enter numerator of the first fraction\n");
-        try {
-            if (sc.hasNextInt()) {
-                int first = sc.nextInt();
-                firstNum = first;
-                System.out.println(first + " :Is the numerator of the first fraction");
-                System.out.println("   Enter denominator of the first fraction");
+        int thirdNum = getInt();
+        System.out.println(thirdNum + " : Is the numerator of the second fraction.");
+        System.out.println("   Enter the denominator of the second fraction:");
 
+        int fourthNum = getInt();
+        System.out.println(fourthNum + " Is the denominator of the second fraction.");
+        System.out.println("The fractions looks like : " + firstNum + "/" + secondNum + " and " + thirdNum + "/" + fourthNum);
+        System.out.println("Choose what you want to do with the fractions:");
+        System.out.println("Multiply: enter *");
+        System.out.println("Divide: enter /");
+        System.out.println("Add: enter +");
+        System.out.println("Subtract: enter -");
 
-                if (!sc.hasNextInt()) {
+        char operation = getOperation();
+        int resultUp = numerator(firstNum, secondNum, thirdNum, fourthNum, operation);
+        int resultDown = denominator(secondNum, thirdNum, fourthNum, operation);
 
+        sc.close();
 
-                } else {
-                    int second = sc.nextInt();
-                    secondNum = second;
-                    System.out.println(secondNum + " :Is the denominator of the first fraction");
-                    System.out.println("   Now the first fraction looks like this: " + first + "/" + second + "\n");
-
-                    System.out.println("   Enter the numerator of the second fraction");
-
-                    if (!sc.hasNextInt()) {
-                        System.out.println("Enter a possitive integer");
-                    } else {
-                        int third = sc.nextInt();
-                        thirdNum = third;
-                        System.out.println(third + " : Is the numerator of the second fraction");
-                        System.out.println("   Enter the denominator of the second fraction");
-
-                        if (!sc.hasNextInt()) {
-                            System.out.println("Enter a possitive integer");
-                        } else {
-                            int fourth = sc.nextInt();
-                            fourthNum = fourth;
-                            System.out.println(fourth + " Is the denominator of the second fraction");
-
-                            System.out.println("The fractions looks like : " + first + "/" + second + " and " + third + "/" + fourth);
-                            System.out.println("Choose what you want to do with the fractions:\n");
-                            System.out.println("Muptiply: enter *");
-                            System.out.println("Divide: enter /");
-                            System.out.println("Append: enter +");
-                            System.out.println("Substract: enter -");
-                            System.out.println("Enter operation in the the specified sequence. AS ABOVE\n");
-
-                            if (sc.next().equals("*")) {
-                                upperResult = first * third;
-                                lowerResult = second * fourth;
-                                System.out.println("The answer of multiplying fractions is: "
-                                        + upperResult + "/" + lowerResult);
-
-                                if (sc.next().equals("/"))
-
-                                    upperResult = first * fourth;
-                                lowerResult = second * third;
-                                System.out.println("The answer of divining fractions is: "
-                                        + upperResult + "/" + lowerResult);
-
-
-                                if (sc.next().equals("+"))
-                                    lowerResult = second * fourth;
-                                upperResult = first * fourth + second * third;
-                                System.out.println("The answer of appending fractions is: "
-                                        + upperResult + "/" + lowerResult);
-
-                                if (sc.next().equals("-"))
-                                    lowerResult = second * fourth;
-                                upperResult = first * fourth - second * third;
-                                System.out.println("The answer of substracting fractions is: "
-                                        + upperResult + "/" + lowerResult);
-
-
-                            }
-                        }
-                    }
-                }
-
-            }
-        } catch (Exception e) {
-            System.out.println("Input a integer!");
-
-
-        }
+        System.out.println("The answer is :" + resultUp + "/" + resultDown);
 
     }
+
+    public static int getInt() {
+        int num;
+        if (sc.hasNextInt()) {
+            num = sc.nextInt();
+        } else {
+            System.out.println("You have entered not a number. Please try again");
+            sc.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation() {
+        System.out.println("Enter operation:");
+        char operation;
+        if (sc.hasNext()) {
+            operation = sc.next().charAt(0);
+        } else {
+            System.out.println("You made a mistake while entering the operation. Please try again.");
+            sc.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int numerator(int firstNum, int secondNum, int thirdNum, int fourthNum, char operation) {
+        int resultUp;
+        switch (operation) {
+            case '+':
+                resultUp = firstNum * fourthNum + secondNum * thirdNum;
+                return resultUp;
+            case '-':
+                resultUp = firstNum * fourthNum - secondNum * thirdNum;
+                return resultUp;
+            case '*':
+                resultUp = firstNum * thirdNum;
+                return resultUp;
+            case '/':
+                resultUp = firstNum * fourthNum;
+                return resultUp;
+            default:
+                System.out.println("The operation is not recognized. Repeat the input.");
+                resultUp = numerator(firstNum, secondNum, thirdNum, fourthNum, getOperation());
+        }
+        return resultUp;
+    }
+
+    public static int denominator(int secondNum, int thirdNum, int fourthNum, char operation) {
+        int resultDown;
+        switch (operation) {
+            case '+':
+                resultDown = secondNum * fourthNum;
+                return resultDown;
+            case '-':
+                resultDown = secondNum * fourthNum;
+                return resultDown;
+            case '*':
+                resultDown = secondNum * fourthNum;
+                return resultDown;
+            case '/':
+                resultDown = secondNum * thirdNum;
+                return resultDown;
+            default:
+                System.out.println("The operation is not recognized. Repeat the input.");
+                resultDown = denominator(secondNum, thirdNum, fourthNum, getOperation());
+
+        }
+        return resultDown;
+    }
+
 }
-
---
+++ CalculateTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+    @Test
+    public void getInt() throws Exception {
+
+    }
+
+    @Test
+    public void getOperation() throws Exception {
+
+    }
+
+    @Test
+    public void numerator() throws Exception {
+    }
+
+    @Test
+    public void denominator() throws Exception {
+    }
+
+}

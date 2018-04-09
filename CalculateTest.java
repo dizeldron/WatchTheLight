0318e200b0446bbc3ea0ad81be8d43bda00b08d0
Andrew Kamendrovsky <32849000+dizeldron@users.noreply.github.com>
2018-04-09 06:38:03 +0300

    Add files via upload

--- CalculateTest.java
+++ CalculateTest.java
@@ -1,24 +1,71 @@
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CalculateTest {
+    Calculate calc = new Calculate();
+
+       //Testing the numerator operations
+          @Test
+    public void numeratorPlusTest()  {
+        double result = calc.numerator(123,234,345,456,
+                        Calculate.setOperationPlus() );
+                        assertEquals( 136818, result,1 );
+
+    }
     @Test
-    public void getInt() throws Exception {
+    public void numeratorMinusTest()  {
+        double result = calc.numerator(123,234,345,456,
+                        Calculate.setOperationMinus() );
+                        assertEquals( -24642, result,1 );
+
+    }
+    @Test
+    public void numeratorDivineTest()  {
+        double result = calc.numerator(123,234,345,456,
+                Calculate.setOperationDivine() );
+                assertEquals( 56088.0, result,1 );
+
+    }
+    @Test
+    public void numeratorMultiplyTest()  {
+        double result = calc.numerator(123,234,345,456,
+                Calculate.setOperationMultiply() );
+                assertEquals( 42435, result,1 );
+
+    }
+
+   //Testing the denominator operations
+
+    @Test
+    public void denominatorPlusTest() throws Exception {
+        double result = calc.denominator(234,345,456,
+                Calculate.setOperationPlus() );
+                assertEquals( 106704, result,1 );
 
     }
 
     @Test
-    public void getOperation() throws Exception {
+    public void denominatorMinusTest() throws Exception {
+        double result = calc.denominator(234,345,456,
+                Calculate.setOperationMinus() );
+                assertEquals( 106704, result,1 );
 
     }
 
     @Test
-    public void numerator() throws Exception {
+    public void denominatorDivineTest() throws Exception {
+        double result = calc.denominator(234,345,456,
+                Calculate.setOperationDivine() );
+                assertEquals( 80730, result,0 );
+
     }
 
     @Test
-    public void denominator() throws Exception {
+    public void denominatorMultiplyTest() throws Exception {
+        double result = calc.denominator(234,345,456,
+                Calculate.setOperationMultiply() );
+                assertEquals( 106704, result,1 );
+
     }
 
 }
